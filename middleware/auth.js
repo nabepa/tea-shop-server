674c317f69444a01ea37f150351e0be20c39bc15
nabepa <nabepadev@gmail.com>
2021-07-12 22:57:18 +0900

    check admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -24,3 +24,28 @@ export const isAuth = async (req, res, next) => {
     next();
   });
 };
+
+// Todo: isAuth에 user.admin 확인하는 것만 추가 된 미들웨어, 코드 중복 막는 법 없을까?
+export const isAdmin = async (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!(authHeader && authHeader.startsWith('Bearer '))) {
+    return res.status(401).json(AUTH_ERROR);
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
+    if (error) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    const user = await userRepository.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    if (!user.admin) {
+      return res.sendStatus(403);
+    }
+    req.userId = user.id;
+    req.token = token;
+    next();
+  });
+};
--- router/products.js
+++ router/products.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import 'express-async-errors';
+import { isAdmin } from '../middleware/auth.js';
 import * as productController from '../controller/products.js';
 
 const router = express.Router();
 
-router.get('/', productController.getProducts);
-router.get('/:id', productController.getProduct);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.removeProduct);
+// Todo: Product validation
+
+router.get('/', isAdmin, productController.getProducts);
+router.get('/:id', isAdmin, productController.getProduct);
+router.post('/', isAdmin, productController.createProduct);
+router.put('/:id', isAdmin, productController.updateProduct);
+router.delete('/:id', isAdmin, productController.removeProduct);
 
 export default router;
